8c564ceb3a52c8e045cc8fb9a7083842f59bd005
CamachoJon <jonathan.camacho@epita.fr>
2023-05-03 14:07:15 +0200

    Added final graph from my side

--- graphs.py
+++ graphs.py
@@ -69,13 +69,30 @@ graph_3 = px.violin(anime_df, y="score", x="airing", color="status", box=True,
 # Fourth graph
 # TODO add changes to visualize different 
 scrapped_df['Sales (Million)'] = scrapped_df['Sales (Million)'].astype('float')
-scrapped_df['Average Sales Per Volume (Million)'] = scrapped_df['Average Sales Per Volume (Million)'].astype('float')
+scrapped_df['Average Sales Per Volume (Million)'] = scrapped_df['Average Sales Per Volume (Million)'].astype('float') * 100
 
 graph_4 = px.scatter(scrapped_df, x = "score", y = "scored_by", size = "Sales (Million)", color = "source",
-           hover_name = "title", log_x = True, size_max = 60)
+           hover_name = "title", log_x = True, size_max = 40)
+
+# fifth Graph
+# bin the score column into discrete intervals
+anime_df['score_bin'] = pd.cut(anime_df['score'], bins=[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+
+# create a pivot table to count the number of shows by rating and score interval
+rating_grading_df = anime_df.pivot_table(index='rating', columns='score_bin', values='title', aggfunc='count').reset_index()
+rating_grading_df.columns = ['rating', '0-1', '1-2', '2-3', '3-4', '4-5', '5-6', '6-7', '7-8', '8-9', '9-10']
+
+graph_5 = px.bar(rating_grading_df, x='rating', y='5-6')
 
 app.layout = html.Div(children=[
     html.H1(children='Charts about different Animes and Mangas'),
+
+
+
+    dcc.Graph(
+        id='graph-1',
+        figure=graph_1
+    ),
 
     html.Div([
 
@@ -98,19 +115,43 @@ app.layout = html.Div(children=[
     ]),
 
     dcc.Graph(
-        id='graph-1',
-        figure=graph_1
-    ),
-
-    dcc.Graph(
         id='graph-3',
         figure=graph_3
     ),
 
-    dcc.Graph(
-        id='graph-4',
-        figure=graph_4
-    )
+    html.Div([
+        dcc.Dropdown(id='dropdown-sales',
+                    options=[{'label': 'Sales', 'value': 'sales'},
+                             {'label': 'Sales per Volume', 'value': 'sales_volume'}],
+                             value='sales'),
+
+        dcc.Graph(
+            id='graph-4',
+            figure=graph_4
+        )
+    ]),
+
+    html.Div([
+
+        dcc.Dropdown(id='dropdown-count-scores',
+                    options=[
+                        {'label': '0-1', 'value': '0-1'},
+                        {'label': '1-2', 'value': '1-2'},
+                        {'label': '2-3', 'value': '2-3'},
+                        {'label': '3-4', 'value': '3-4'},
+                        {'label': '4-5', 'value': '4-5'},
+                        {'label': '5-6', 'value': '5-6'},
+                        {'label': '6-7', 'value': '6-7'},
+                        {'label': '7-8', 'value': '7-8'},
+                        {'label': '8-9', 'value': '8-9'},
+                        {'label': '9-10', 'value': '9-10'}],
+                        value='5-6'),
+
+        dcc.Graph(
+            id='graph-5',
+            figure=graph_5
+        )
+    ])
 
 ])
 
@@ -119,23 +160,32 @@ app.layout = html.Div(children=[
     Input('dropdown-ratings', 'value'))
 
 def update_graph(value):
-
-    if value == 'score':
-        graph_2.update_traces(x=anime_df['score'])
-    elif value == 'scored_by':
-        graph_2.update_traces(x=anime_df['scored_by'])
-    elif value == 'rank':
-        graph_2.update_traces(x=anime_df['rank'])
-    elif value == 'popularity':
-        graph_2.update_traces(x=anime_df['popularity'])
-    elif value == 'members':
-        graph_2.update_traces(x=anime_df['members'])
-    elif value == 'favorites':
-        graph_2.update_traces(x=anime_df['favorites'])
-
+    graph_2.update_traces(x=anime_df[value])
     graph_2.update_layout(xaxis={'title': f'{value}'})
     return graph_2
 
+@app.callback(
+    Output('graph-4', 'figure'),
+    Input('dropdown-sales', 'value')
+)
+
+def update_graph_sales(value):
+    if value == 'sales':
+        graph_4.update_traces(marker=dict(size=scrapped_df['Sales (Million)']))
+    elif value == 'sales_volume':
+        graph_4.update_traces(marker=dict(size=scrapped_df['Average Sales Per Volume (Million)']))
+
+    graph_4.update_layout(xaxis={'title': f'{value}'})
+    return graph_4
+
+@app.callback(
+    Output('graph-5', 'figure'),
+    Input('dropdown-count-scores', 'value')
+)
+
+def update_bar_graph(value):
+    graph_5.update_traces(y=rating_grading_df[value])
+    return graph_5
 
 if __name__ == '__main__':
     app.run_server(debug=True)
